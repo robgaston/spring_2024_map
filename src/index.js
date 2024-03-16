19d6e8350fc5e3fba024544b979e2c57e881984b
Rob Gaston <rgaston@fargeo.com>
2024-03-16 19:40:34 +0000

    adds day 3 work

--- src/index.js
+++ src/index.js
@@ -21,4 +21,59 @@ map.on("load", async () => {
 
     map.getSource("neighborhoods-polygons")
         .setData(neighborhoods);
+
+    const popup = document.querySelector("#popup");
+
+    map.on("mousemove", "neighborhoods-fill", (event) => {
+        const hoveredFeature = event.features[0];
+
+        popup.querySelector(".neighborhood-name").textContent = hoveredFeature.properties.name;
+        popup.querySelector(".count-content").textContent = hoveredFeature.properties.count.toLocaleString();
+
+        popup.style.display = "block";
+
+        map.removeFeatureState({
+            source: "neighborhoods-polygons"
+        });
+        map.setFeatureState({
+            source: "neighborhoods-polygons",
+            id: hoveredFeature.id
+        }, {
+            hover: true
+        });
+    });
+
+    map.on("mouseleave", "neighborhoods-fill", (event) => {
+        popup.style.display = "none";
+
+        map.removeFeatureState({
+            source: "neighborhoods-polygons"
+        });
+    });
+
+    const legend = document.querySelector("#legend");
+    const entryTemplate = document.querySelector("#legend-entry");
+    const fillColorStyle = map.getPaintProperty("neighborhoods-fill", "fill-extrusion-color");
+
+    fillColorStyle.splice(0, 2);
+    let fromValue = 0;
+
+    for (let index = 0; index < fillColorStyle.length; index+=2) {
+        const entry = document.importNode(entryTemplate.content, true);
+        const color = entry.querySelector(".color");
+        const range = entry.querySelector(".range");
+        const toValue = fillColorStyle[index+1];
+
+        color.style.backgroundColor = fillColorStyle[index];
+
+        if (index==fillColorStyle.length-1) {
+            range.textContent = `>=${fromValue}`;
+        } else {
+            range.textContent = `${fromValue}-${toValue-1}`;
+        }
+
+        fromValue = toValue;
+
+        legend.appendChild(entry);
+    }
 });
